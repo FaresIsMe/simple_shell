21de96d24a8f3f3fa61ef5c75a5775b18ced5946
AMR856 <amer.live477@gmail.com>
2023-09-10 17:24:00 +0300

    Just testing vscode

--- lists_functions_1.c
+++ lists_functions_1.c
@@ -28,5 +28,102 @@ size_t print_list(const list_t *h)
 	return (counter);
 }
 
+/**
+ * addingNodeBeg - A function to add a node at the beginning of the
+ * linked list
+ * @myHead: A pointer to the head of the linked list
+ * @myString: A pointer to the string that we want to put
+ * in the linked list
+ * @num: The number of the node in the history
+ *
+ * Return: NULL or a pointer to the newly created node
+*/
+
+myList *addingNodeBeg(myList *myHead, const char *myString, int num)
+{
+	myList *newHead;
+	if (myHead == NULL)
+		return (NULL);
+	newHead = malloc(sizeof(myList));
+	if (newHead == NULL)
+		return (NULL);
+	_memset(void *)newHead, 0, sizeof(myList);
+	(*newHead).theNum = num;
+	if (myString != NULL)
+	{
+		(*newHead).theString = _strdup(myString);
+		if ((*newHead).theString == NULL)
+		{
+			free(newHead);
+			return (NULL);
+		}
+	}
+	(*newHead).next = *myHead;
+	*myHead = newHead;
+	return(newHead);
+}
+
+/**
+ * add_node_end - A function to add a node at the end
+ * @head: A pointer to the pointer who points at the first node
+ * @str: A string the should be inputed into the newly created node
+ * @num: The number of the node in the history
+ * Return: It returns a pointer to the newly created node
+*/
+
+list_t *add_node_end(list_t **head, const char *str, int num)
+{
+	myList *newNode = (myList *)malloc(sizeof(myList));
+	myList *current;
+	
+	if (newNode == NULL || head == NULL)
+		return (NULL);
+	_memset((void *)newNode, 0, sizeof(newNode));
+	(newNode).theNum = num;
+	if ((*newNode).theString != NULL)
+	{
+		newNode->str = _strdup(str);
+		newNode->next = NULL;
+	}
+	else
+	{
+		free(newNode);
+		return (NULL);
+	}
+	if (*head == NULL)
+	{
+		*head = newNode;
+		return (newNode);
+	}
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+	current->next = newNode;
+	return (newNode);
+}
 
 
+/**
+ * free_list - A function to free the linked list
+ * @head: A pointer to a pointer that
+ * points to the head of the linked list
+ * 
+ * Return: It returns nothing (void)
+*/
+
+void free_list(myList **head)
+{
+	list_t *current = *head;
+	list_t *next;
+	
+	if (head == NULL || *head == NULL)
+		return (NULL);
+	while (current != NULL)
+	{
+		next = current->next;
+		free(current->theString);
+		free(current);
+		current = next;
+	}
+	*head = NULL;
+}
--- shell.h
+++ shell.h
@@ -9,6 +9,14 @@
 #define MAX_BUFFER_SIZE 1024 /* max size of the buffer */
 #define BUFFER_FLUSH_CONDITION -1 /* the condition when we flush */
 
+
+typedef struct myNewList
+{
+	int theNum;
+	char *myString;
+	struct myNewList *next;
+} myList;
+
 /*string_function_1*/
 int _strlen(char *str);
 int _strcmp(char *str1, char *str2);
@@ -16,12 +24,27 @@ char *_strcat(char *str1, const char *str2);
 char *_strcpy(char *str1, char *str2);
 char *_strncpy(char *str1, char *str2, int n);
 
+/*string_functions_2*/
+char *_strdup(char *str);
+char *startsWith(const char *myString, const char *theSString);
+char *_strchr(const char *myString, char myChar);
+
 /*string_functions_3*/
 char *_strncat(char *dest, char *src, int n);
 void _puts(char *s);
 int _putchar(char c);
+
+/*memory_functions_1*/
+char *_memset(char *s, char b, unsigned int n);
+void freeAString(char **myPointer);
+bool freeNulling(char **myPoitner);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 
+/*additional_functions_1*/
+bool isInteractive(myInfoObject *myInfo);
+bool _isalpha(int myChar);
+bool isDelim(char checkedOne, char *theStringDel);
+int _atoi(char *s);
 
 
 #endif
