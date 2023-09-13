1e3ed157fa94e2148b7ea006bf985fda5f1fc33d
Fares Ahmed <fareselzeny6@gmail.com>
2023-09-14 00:24:09 +0300

    betty is a pity

--- string_functions_4.c
+++ string_functions_4.c
@@ -8,15 +8,13 @@
  */
 char **strtow2(char *str, char *del)
 {
-	if (str == NULL || del == NULL)
-		return (NULL);
-
-	int i, j, k, n, m , count = 0;
+	int i, j, k, n, m, count = 0, len = 0;
 	char **words = NULL;
 
+	if (str == NULL || del == NULL)
+		return (NULL);
 	n = _strlen(str);
 	m = _strlen(del);
-
 	for (i = 0; i < n; i++)
 	{
 		for (j = 0; j < m; j++)
@@ -27,12 +25,9 @@ char **strtow2(char *str, char *del)
 		if (j == m && (i == 0 || j != m - 1 && str[i - 1] == del[m - 1]))
 			count++;
 	}
-
-
 	words = (char **) malloc((count + 1) * sizeof(char *));
 	if (words == NULL)
 		return (NULL);
-
 	for (i = 0, j = 0; i < n && j < count; i++)
 	{
 		for (k = 0; k < m; k++)
@@ -42,7 +37,7 @@ char **strtow2(char *str, char *del)
 		}
 		if (k == m && (i == 0 || k != m - 1 && str[i - 1] == del[m - 1]))
 		{
-			int len = i - j;
+			len = i - j;
 			words[j] = (char *) malloc((len + 1) * sizeof(char));
 			if (words[j] == NULL)
 				return (NULL);
@@ -52,7 +47,6 @@ char **strtow2(char *str, char *del)
 		}
 	}
 	words[count] = NULL;
-
 	return (words);
 }
 
@@ -85,47 +79,38 @@ int is_del(char c, char *del)
 char **strtow(char *str, char *del)
 {
 	char **words = NULL;
-	int num_words = 0, str_len, del_len, word_start, word_len;
+	int num_words = 0, str_len, del_len, word_start, word_len, i = 0, word_len;
 
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
-
 	if (del == NULL)
 		del = " ";
-
 	str_len = _strlen(str);
 	del_len = _strlen(del);
-
-	int i = 0;
 	while (i < str_len)
 	{
 		while (i < str_len && is_del(str[i], del))
 			i++;
-
 		if (i == str_len)
 			break;
-
 		word_start = i;
 		while (i < str_len && !is_del(str[i], del))
 			i++;
-
-		int word_len = i - word_start;
+		word_len = i - word_start;
 		num_words++;
-
-		words = _realloc(words, num_words * sizeof(char *), (num_words + 1) * sizeof(char *));
+		words = _realloc(words, num_words * sizeof(char *),
+		(num_words + 1) * sizeof(char *));
 		words[num_words - 1] = malloc((word_len + 1) * sizeof(char));
 		_strncpy(words[num_words - 1], str + word_start, word_len);
 		words[num_words - 1][word_len] = '\0';
 	}
-
 	if (num_words == 0)
 	{
 		free(words);
 		return (NULL);
 	}
-
-	words = _realloc(words, (num_words + 1) * sizeof(char *), (num_words + 1) * sizeof(char *));
+	words = _realloc(words, (num_words + 1) * sizeof(char *),
+	(num_words + 1) * sizeof(char *));
 	words[num_words] = NULL;
-
 	return (words);
 }
