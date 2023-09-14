a937cbac437d201c6ee380629cdec9eee7fad08b
Fares Ahmed <fareselzeny6@gmail.com>
2023-09-14 20:32:31 +0300

    Snake snakey

---
+++ builtin_ASS_functions_1.c
@@ -0,0 +1,88 @@
+#include "shell.h"
+
+/**
+ * _exit_shell - it exits the damn shell
+ * @myInfo: structure contains good information fo easier use
+ * 
+ * Return: -2 and exits the damn shell!
+*/
+
+int _exit_shell(myInfoObject *myInfo)
+{
+    int get_the_fuck_out_check;
+
+    if (myInfo->arguments[1])
+    {
+        get_the_fuck_out_check = errorToInteger(myInfo->arguments[1]);
+        if (get_the_fuck_out_check == -1)
+        {
+            myInfo->status = 2;
+            printingErros(myInfo, "Illegal number: ");
+            _puts(myInfo->arguments[1]);
+            _putchar('\n');
+        }
+        myInfo->error_number = errorToInteger(myInfo->arguments[1]);
+        return (-2);
+    }
+    myInfo->error_number = -1;
+    return (-2);
+}
+
+/**
+ * 
+ * 
+ * 
+ * 
+*/
+
+int _cd_(myInfoObject *myInfo)
+{
+    char *current_dir, *target_dir, FARES[1024];
+    int AMR_dir;
+
+    current_dir = getcwd(FARES, 1024);
+
+    if (!current_dir)
+    {
+         _puts("TODO: >>getcwd failure emsg here<<\n");
+
+        if (!myInfo->arguments[1])
+        target_dir = gettingEnviormentVariable(FARES, 1024);
+
+        if (!target_dir)
+        target_dir = gettingEnviormentVariable(myInfo, "HOME=");
+
+        AMR_dir = chdir(target_dir ? target_dir : "/");
+    }
+    else if (_strcmp(myInfo->arguments[1], "-") == 0)
+    {
+        target_dir = gettingEnviormentVariable(myInfo, "OLDPWD=");
+        if (!target_dir)
+        {
+            _puts(current_dir);
+            _putchar('\n');
+            return (1);
+        }
+        _puts(target_dir);
+        _putchar('\n');
+        AMR_dir = chdir(target_dir);
+    }
+    else
+    {
+        target_dir = myInfo->arguments;
+        AMR_dir = chdir(target_dir);
+    }
+    if (AMR_dir == -1)
+    {
+        printingErros(myInfo, "can't cd to ");
+		_eputs(target_dir);
+		_eputchar('\n');
+    }
+    else
+    {
+        settingEnvVar(myInfo, "OLDPWD", gettingEnviormentVariable(myInfo, "PWD="));
+        settingEnvVar(myInfo, "PWD", getcwd(FARES, "PWD="));
+    }
+    return (0);
+
+}
