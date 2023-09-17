6386b43f8d48d508f3c5fe8917db1c189a2f5112
Fares Ahmed <fareselzeny6@gmail.com>
2023-09-17 22:03:07 +0300

    syntax is finished

--- string_functions_4.c
+++ string_functions_4.c
@@ -8,7 +8,7 @@
  */
 char **strtow2(char *str, char *del)
 {
-	int i, j, k, n, m, count = 0, len = 0;
+	int i, j, k, n, m, count = 0, len = 0, l;
 	char **words = NULL;
 
 	if (str == NULL || del == NULL)
@@ -22,10 +22,10 @@ char **strtow2(char *str, char *del)
 			if (str[i] == del[j])
 				break;
 		}
-		if (j == m && (i == 0 || j != m - 1 && str[i - 1] == del[m - 1]))
+		if (j == m && (i == 0 || (j != m - 1 && str[i - 1] == del[m - 1])))
 			count++;
 	}
-	words = (char **) malloc((count + 1) * sizeof(char *));
+	words = (char **)malloc((count + 1) * sizeof(char *));
 	if (words == NULL)
 		return (NULL);
 	for (i = 0, j = 0; i < n && j < count; i++)
@@ -35,12 +35,17 @@ char **strtow2(char *str, char *del)
 			if (str[i] == del[k])
 				break;
 		}
-		if (k == m && (i == 0 || k != m - 1 && str[i - 1] == del[m - 1]))
+		if (k == m && (i == 0 || (k != m - 1 && str[i - 1] == del[m - 1])))
 		{
 			len = i - j;
-			words[j] = (char *) malloc((len + 1) * sizeof(char));
+			words[j] = (char *)malloc((len + 1) * sizeof(char));
 			if (words[j] == NULL)
+			{
+				for (l = 0; l < j; l++)
+					free(words[l]);
+				free(words);
 				return (NULL);
+			}
 			_strncpy(words[j], str + j, len);
 			words[j][len] = '\0';
 			j++;
@@ -50,9 +55,6 @@ char **strtow2(char *str, char *del)
 	return (words);
 }
 
-
-
-
 /**
  * strtow - splits a string into words. Repeat delimiters are ignored
  * @str: the input string
@@ -60,10 +62,11 @@ char **strtow2(char *str, char *del)
  *
  * Return: a pointer to an array of strings, or NULL on failure
  */
+
 char **strtow(char *str, char *del)
 {
 	char **words = NULL;
-	int num_words = 0, str_len, del_len, word_start, word_len, i = 0, word_len;
+	int num_words = 0, str_len, del_len, word_start, i = 0, word_len, j;
 
 	if (str == NULL || str[0] == '\0')
 		return (NULL);
@@ -71,6 +74,7 @@ char **strtow(char *str, char *del)
 		del = " ";
 	str_len = _strlen(str);
 	del_len = _strlen(del);
+	(void)del_len;
 	while (i < str_len)
 	{
 		while (i < str_len && is_del(str[i], del))
@@ -83,8 +87,23 @@ char **strtow(char *str, char *del)
 		word_len = i - word_start;
 		num_words++;
 		words = _realloc(words, num_words * sizeof(char *),
-		(num_words + 1) * sizeof(char *));
+						 (num_words + 1) * sizeof(char *));
+		if (words == NULL)
+		{
+
+			for (j = 0; j < num_words - 1; j++)
+				free(words[j]);
+			free(words);
+			return (NULL);
+		}
 		words[num_words - 1] = malloc((word_len + 1) * sizeof(char));
+		if (words[num_words - 1] == NULL)
+		{
+			for (j = 0; j < num_words - 1; j++)
+				free(words[j]);
+			free(words);
+			return (NULL);
+		}
 		_strncpy(words[num_words - 1], str + word_start, word_len);
 		words[num_words - 1][word_len] = '\0';
 	}
@@ -94,7 +113,14 @@ char **strtow(char *str, char *del)
 		return (NULL);
 	}
 	words = _realloc(words, (num_words + 1) * sizeof(char *),
-	(num_words + 1) * sizeof(char *));
+					 (num_words + 1) * sizeof(char *));
+	if (words == NULL)
+	{
+		for (j = 0; j < num_words; j++)
+			free(words[j]);
+		free(words);
+		return (NULL);
+	}
 	words[num_words] = NULL;
 	return (words);
 }
